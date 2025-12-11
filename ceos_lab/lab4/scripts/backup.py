f64d20780200aa0fdc006738b9d9b2b8c43fe066
Todd Riemenschneider <twr14152@gmail.com>
2025-12-11 05:31:32 -0500

    Add backup script for network device configuration

---
+++ ceos_lab/lab4/scripts/backup.py
@@ -0,0 +1,17 @@
+from jsonrpclib import Server
+import ssl
+import json
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+hosts = ["leaf1a", "leaf1b", "leaf2", "spine1", "spine2"]
+
+cmds = ["enable", "copy running-config startup-config"]
+
+for host in hosts:
+    print(f"Connecting to host {host}...")
+    switch = Server(f"https://admin:admin@{host}/command-api")
+    response = switch.runCmds( 1, cmds )
+    print(json.dumps(response, indent=2))
+    print(f"Disconnecting from  host {host}...")
+
