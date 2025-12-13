9d08715e192cd8db2a9ddfb01fddcb3d8f8b9a0d
Todd Riemenschneider <twr14152@gmail.com>
2025-12-13 08:07:52 -0500

    Add lab_script.py for device login and command execution

---
+++ ceos_lab/lab4/scripts/lab_script.py
@@ -0,0 +1,160 @@
+#!/home/todd/Code_folder/containerlab/containerlabs_sandbox/myenv/bin/python
+
+from netmiko import ConnectHandler 
+
+devices = ["leaf1a", "leaf1b", "leaf2", "spine1", "spine2"]
+
+q1 = input("Login into all devices?: (y/n)")
+q2 = input("Login into leaf1a: (y/n)")
+q3 = input("Login into leaf1b: (y/n)")
+q4 = input("Login into leafb: (y/n)")
+q5 = input("Login into spine1: (y/n)")
+q6 = input("Login into spine2: (y/n)")
+
+
+
+if len(q1) < 1: q1 == "n"
+if len(q2) < 1: q2 == "n"
+if len(q3) < 1: q3 == "n"
+if len(q4) < 1: q4 == "n"
+if len(q5) < 1: q5 == "n"
+if len(q6) < 1: q6 == "n"
+
+if q1 == "y":
+    config_commands = input("Enter configs: ")
+    show_commands = input("Enter show commands: ")
+    for host in devices:
+        device = {
+        'device_type': 'arista_eos',
+        'ip': host,
+        'username': 'admin',
+        'password': 'admin',
+        'secret' : 'admin',
+        }
+        print(f"connecting to: {host}" )
+        #commands = input("Enter commands: ")
+        cfg_cmds = config_commands.split(",")
+        show_cmds = show_commands.split(",")
+        net_connect = ConnectHandler(**device)
+        net_connect.enable()
+        output = net_connect.send_config_set(cfg_cmds)
+        print(output)
+        for cmd in show_cmds:
+            output = net_connect.send_command(cmd)
+            print(output)
+
+elif q2 == "y":
+    host = "leaf1a"
+    device = {
+            'device_type': 'arista_eos',
+            'ip': host,
+            'username': 'admin',
+            'password': 'admin',
+            'secret' : 'admin',
+            }
+    print(f"connecting to: {host}" )
+#   commands = input("Enter commands: ")
+    config_commands = input("Enter configs: ")
+    show_commands = input("Enter show commands: ")
+    cfg_cmds = config_commands.split(",")
+    show_cmds = show_commands.split(",")
+    net_connect = ConnectHandler(**device)
+    net_connect.enable()
+    output = net_connect.send_config_set(cfg_cmds)
+    print(output)
+    for cmd in show_cmds:
+        output = net_connect.send_command(cmd)
+        print(output)
+
+
+elif q3 == "y":
+    host = "leaf1b"
+    device = {
+            'device_type': 'arista_eos',
+            'ip': host,
+            'username': 'admin',
+            'password': 'admin',
+            'secret' : 'admin',
+            }
+    print(f"connecting to: {host}" )
+    config_commands = input("Enter configs: ")
+    show_commands = input("Enter show commands: ")
+    cfg_cmds = config_commands.split(",")
+    show_cmds = show_commands.split(",")
+    net_connect = ConnectHandler(**device)
+    net_connect.enable()
+    output = net_connect.send_config_set(cfg_cmds)
+    print(output)
+    for cmd in show_cmds:
+        output = net_connect.send_command(cmd)
+        print(output)
+
+elif q4 == "y":
+    host = "leaf2"
+    device = {
+            'device_type': 'arista_eos',
+            'ip': host,
+            'username': 'admin',
+            'password': 'admin',
+            'secret' : 'admin',
+            }
+    print(f"connecting to: {host}" )
+    config_commands = input("Enter configs: ")
+    show_commands = input("Enter show commands: ")
+    cfg_cmds = config_commands.split(",")
+    show_cmds = show_commands.split(",")
+    net_connect = ConnectHandler(**device)
+    net_connect.enable()
+    output = net_connect.send_config_set(cfg_cmds)
+    print(output)
+    for cmd in show_cmds:
+        output = net_connect.send_command(cmd)
+        print(output)
+
+
+
+elif q5 == "y":
+    host = "spine1"
+    device = {
+            'device_type': 'arista_eos',
+            'ip': host,
+            'username': 'admin',
+            'password': 'admin',
+            'secret' : 'admin',
+            }
+    print(f"connecting to: {host}" )
+    config_commands = input("Enter configs: ")
+    show_commands = input("Enter show commands: ")
+    cfg_cmds = config_commands.split(",")
+    show_cmds = show_commands.split(",")
+    net_connect = ConnectHandler(**device)
+    net_connect.enable()
+    output = net_connect.send_config_set(cfg_cmds)
+    print(output)
+    for cmd in show_cmds:
+        output = net_connect.send_command(cmd)
+        print(output)
+
+
+elif q6 == "y":
+    host = "spine2"
+    device = {
+            'device_type': 'arista_eos',
+            'ip': host,
+            'username': 'admin',
+            'password': 'admin',
+            'secret' : 'admin',
+            }
+    print(f"connecting to: {host}" )
+    config_commands = input("Enter configs: ")
+    show_commands = input("Enter show commands: ")
+    cfg_cmds = config_commands.split(",")
+    show_cmds = show_commands.split(",")
+    net_connect = ConnectHandler(**device)
+    net_connect.enable()
+    output = net_connect.send_config_set(cfg_cmds)
+    print(output)
+    for cmd in show_cmds:
+        output = net_connect.send_command(cmd)
+        print(output)
+
