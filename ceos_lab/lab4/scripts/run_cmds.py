bb178e694d9f94266d47cb1d9ac6ea627b42c90b
Todd Riemenschneider <twr14152@gmail.com>
2025-12-18 06:19:52 -0500

    Update run_cmds.py

--- ceos_lab/lab4/scripts/run_cmds.py
+++ ceos_lab/lab4/scripts/run_cmds.py
@@ -12,10 +12,10 @@ if ans == 'y':
     hosts = ["leaf1a", "leaf1b", "leaf2", "spine1", "spine2"]
     ans = input("Do you have a config or cmds file to load (y/n): ")
     if ans == 'y':
+        cfg_file = input("Enter file name (eg..'hostname.cmds' or 'hostname-description.cfg'): ")
+        with open(cfg_file) as f:
+            cmds = f.read().splitlines()
         for host in hosts:
-            cfg_file = input("Enter file name (eg..'hostname.cmds' or 'hostname-description.cfg'): ")
-            with open(cfg_file) as f:
-                cmds = f.read().splitlines()
             url = f"https://{host}/command-api"
             print(f"Connected to {host}..")
             headers = {'Content-Type': 'application/json'}
@@ -85,25 +85,3 @@ else:
             print(json.dumps(response.json(), indent=2))
             print(f"Closing connection to {host}...\n\n")
 
-
-    else:
-        commands = input('Enter commands to run seperated by ",": ')
-        cmds = commands.split(",")
-
-        for host in hosts:
-            url = f"https://{host}/command-api"
-            print(f"Connected to {host}..")
-            headers = {'Content-Type': 'application/json'}
-            payload = {
-                    "jsonrpc": "2.0",
-                    "method": "runCmds",
-                    "params": {
-                        "version": 1,
-                        "cmds": cmds,
-                        "format": "json"
-                        },
-                    "id": "1"
-                    }
-            response = requests.post(url, data=json.dumps(payload), auth=('admin', 'admin'), verify=False)
-            print(json.dumps(response.json(), indent=2))
-            print(f"Closing connection to {host}...\n\n")
