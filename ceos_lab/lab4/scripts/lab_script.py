93a92ccd3eed0dcdcdbc038a0ea17582caa769d9
Todd Riemenschneider <twr14152@gmail.com>
2025-12-18 07:31:43 -0500

    Update lab_script.py

--- ceos_lab/lab4/scripts/lab_script.py
+++ ceos_lab/lab4/scripts/lab_script.py
@@ -5,28 +5,13 @@ from netmiko import ConnectHandler
 devices = ["leaf1a", "leaf1b", "leaf2", "spine1", "spine2"]
 
 q1 = input("Login into all devices?: (y/n)")
-q2 = input("Login into leaf1a: (y/n)")
-q3 = input("Login into leaf1b: (y/n)")
-q4 = input("Login into leafb: (y/n)")
-q5 = input("Login into spine1: (y/n)")
-q6 = input("Login into spine2: (y/n)")
-
-
-
-if len(q1) < 1: q1 == "n"
-if len(q2) < 1: q2 == "n"
-if len(q3) < 1: q3 == "n"
-if len(q4) < 1: q4 == "n"
-if len(q5) < 1: q5 == "n"
-if len(q6) < 1: q6 == "n"
-
 if q1 == "y":
     config_commands = input("Enter configs: ")
     show_commands = input("Enter show commands: ")
     for host in devices:
         device = {
         'device_type': 'arista_eos',
-        'ip': host,
+        'ip': host, 
         'username': 'admin',
         'password': 'admin',
         'secret' : 'admin',
@@ -42,8 +27,25 @@ if q1 == "y":
         for cmd in show_cmds:
             output = net_connect.send_command(cmd)
             print(output)
+    end_script = input("Are you done (y/n): ")
+    if end_script == "y":
+        sys.exit(0)
 
-elif q2 == "y":
+q2 = input("Login into leaf1a: (y/n)")
+q3 = input("Login into leaf1b: (y/n)")
+q4 = input("Login into leafb: (y/n)")
+q5 = input("Login into spine1: (y/n)")
+q6 = input("Login into spine2: (y/n)")
+
+
+
+if len(q2) < 1: q2 == "n"
+if len(q3) < 1: q3 == "n"
+if len(q4) < 1: q4 == "n"
+if len(q5) < 1: q5 == "n"
+if len(q6) < 1: q6 == "n"
+
+if q2 == "y":
     host = "leaf1a"
     device = {
             'device_type': 'arista_eos',
@@ -53,7 +55,6 @@ elif q2 == "y":
             'secret' : 'admin',
             }
     print(f"connecting to: {host}" )
-#   commands = input("Enter commands: ")
     config_commands = input("Enter configs: ")
     show_commands = input("Enter show commands: ")
     cfg_cmds = config_commands.split(",")
@@ -67,7 +68,7 @@ elif q2 == "y":
         print(output)
 
 
-elif q3 == "y":
+if q3 == "y":
     host = "leaf1b"
     device = {
             'device_type': 'arista_eos',
@@ -89,7 +90,7 @@ elif q3 == "y":
         output = net_connect.send_command(cmd)
         print(output)
 
-elif q4 == "y":
+if q4 == "y":
     host = "leaf2"
     device = {
             'device_type': 'arista_eos',
@@ -113,7 +114,7 @@ elif q4 == "y":
 
 
 
-elif q5 == "y":
+if q5 == "y":
     host = "spine1"
     device = {
             'device_type': 'arista_eos',
@@ -136,7 +137,7 @@ elif q5 == "y":
         print(output)
 
 
-elif q6 == "y":
+if q6 == "y":
     host = "spine2"
     device = {
             'device_type': 'arista_eos',
